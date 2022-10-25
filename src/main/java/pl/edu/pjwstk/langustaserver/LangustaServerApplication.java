983a18a7367b25dd5df7f01d67bc637373511f11
SOURI-HQ <seconddan@wp.pl>
2022-10-25 21:12:22 +0200

    initial commit

---
+++ src/main/java/pl/edu/pjwstk/langustaserver/LangustaServerApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pjwstk.langustaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangustaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangustaServerApplication.class, args);
+	}
+
+}
