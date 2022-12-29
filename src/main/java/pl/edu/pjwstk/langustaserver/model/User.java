6d543659f800d11cf2fcfccb8ddc0710495aa34c
SOURI-HQ <seconddan@wp.pl>
2022-12-29 22:10:31 +0100

    fix: change method names to use camelCase notation

--- src/main/java/pl/edu/pjwstk/langustaserver/model/User.java
+++ src/main/java/pl/edu/pjwstk/langustaserver/model/User.java
@@ -29,11 +29,11 @@ public class User {
         return encoder.matches(password, pwHash);
     }
 
-    public Integer getID() {
+    public Integer getId() {
         return id;
     }
 
-    public void setID(Integer id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
