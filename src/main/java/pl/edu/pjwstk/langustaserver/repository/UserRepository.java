51b1dd195305f2103d58617b56ff44178f5e20a4
mefeleth <zuzzber@gmail.com>
2023-01-12 10:17:56 +0100

    feat(31): Add user repository

---
+++ src/main/java/pl/edu/pjwstk/langustaserver/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package pl.edu.pjwstk.langustaserver.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.edu.pjwstk.langustaserver.model.User;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    User getUserById(String id);
+    void saveUser(User user);
+
+}
