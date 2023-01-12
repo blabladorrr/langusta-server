75d74b637d12c7668711b7455b4619088a76bea9
Michał Siwek <mjzsiwek@gmail.com>
2023-01-12 22:28:10 +0100

    feat(22): Removed unnecessary dependency

--- src/main/java/pl/edu/pjwstk/langustaserver/configuration/LangustaConfiguration.java
+++ src/main/java/pl/edu/pjwstk/langustaserver/configuration/LangustaConfiguration.java
@@ -2,7 +2,6 @@ package pl.edu.pjwstk.langustaserver.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
