37faf0799fac836e2d95283668624cff4ef54ee0
Michał Siwek <mjzsiwek@gmail.com>
2023-01-15 21:32:45 +0100

    feat(34): Deleted unwanted blank linke

--- src/main/java/pl/edu/pjwstk/langustaserver/LangustaServerApplication.java
+++ src/main/java/pl/edu/pjwstk/langustaserver/LangustaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LangustaServerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(LangustaServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(LangustaServerApplication.class, args);
+	}
 }
